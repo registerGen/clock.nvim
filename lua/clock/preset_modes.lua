683738a0d86ee7bfbdb918f9c92848c6162305ed
registerGen <registergen@163.com>
2024-06-16 20:06:49 +0800

    presets: fix collision of hl group names

--- lua/clock/preset_modes.lua
+++ lua/clock/preset_modes.lua
@@ -15,6 +15,8 @@ function M.gradient(hl_groups, level, map)
     b[#b + 1] = bit.band(fg, 255)
   end
 
+  local hash = 0
+
   for i = 1, #hl_groups - 1, 1 do
     for j = 1, level, 1 do
       local R, G, B
@@ -22,16 +24,18 @@ function M.gradient(hl_groups, level, map)
       G = math.floor(g[i] * (level + 1 - j) / level + g[i + 1] * (j - 1) / level + 0.5)
       B = math.floor(b[i] * (level + 1 - j) / level + b[i + 1] * (j - 1) / level + 0.5)
       colors[#colors + 1] = string.format("#%X%X%X", R, G, B)
+      hash = (((hash * 256 + R) * 256 + G) * 256 + B) % 998244353
     end
   end
 
+
   for i = 1, (#hl_groups - 1) * level, 1 do
-    api.nvim_set_hl(0, "Clock" .. tostring(i), { fg = colors[i], bg = "bg" })
+    api.nvim_set_hl(0, "Clock_" .. tostring(hash) .. "_" .. tostring(i), { fg = colors[i], bg = "bg" })
   end
 
   return {
     hl_group_pixel = function(_, position, _, pixel_col)
-      return "Clock" .. tostring(map(position, pixel_col))
+      return "Clock_" .. tostring(hash) .. "_" .. tostring(map(position, pixel_col))
     end,
     hl_group_separator = "Normal",
   }
